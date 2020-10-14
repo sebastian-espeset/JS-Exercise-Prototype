6127df8fd9c8538b4f9efdce0542c10786a586a9
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-14 15:59:40 -0700

    Finished task 1

--- index.js
+++ index.js
@@ -39,9 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+  this.name=name;
+  this.age=age;
+  this.stomach=[];
 }
+Person.prototype.eat=function(str){
+  if(this.stomach.length<10){
+  this.stomach.push(str);
+  }
+}
+Person.prototype.poop=function(){
+  this.stomach=[];
+  }
+Person.prototype.toString=function(){
+  return `${this.name}, ${this.age}`;
+}
+
+//prototype out here for eat method, here's an example of prototyping
+//EnlistKnight.prototype.knightInfo=function(){
+ // console.log(`Name: ${this.name}| Weapon:${this.weapon}| Skill:${this.skill}`);
+//}
 
 /*
   TASK 2
