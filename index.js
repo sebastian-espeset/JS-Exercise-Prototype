5436d0f25def9ef6fe4fb87fb20dbae853f1694b
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-14 16:14:58 -0700

    Finished task 2 without stretches

--- index.js
+++ index.js
@@ -75,8 +75,15 @@ Person.prototype.toString=function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+      this.model=model;
+      this.milesPerGallon=milesPerGallon;
+      this.tank=0;
+      this.odometer=0;
+}
+//prototype function that fills tank
+Car.prototype.fill=function(gallons){
+  this.tank+=gallons;
 }
 
 /*
